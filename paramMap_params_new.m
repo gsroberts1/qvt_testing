ff7c58cebd979570b9d80a24558bb2a2b7854376
Grant Roberts <gsroberts@wisc.edu>
2021-08-25 09:45:05 -0500

    08252021
    Removing erosion for sliding threshold method

--- paramMap_params_new.m
+++ paramMap_params_new.m
@@ -191,10 +191,6 @@ for n = 1:size(Tangent_V,1)
     areaThresh = round(sum(segment(:)).*0.05); %minimum area to keep
     conn = 6; %connectivity (i.e. 6-pt)
     segment = bwareaopen(segment,areaThresh,conn); %inverse fill holes
-    erodeFactor = 1;
-    %if segmentIsNotTooSmall
-        segment = imerode(segment,strel('disk',erodeFactor)); %erode segmentation
-    %end
     % Can compare in-plane segmentation to initial global segmentation. 
     % To do this, the 'segment' variable from 'loadpcvipr' needs to be 
     % passed as an arg. I did this by adding 'segment_old' as 2nd input
